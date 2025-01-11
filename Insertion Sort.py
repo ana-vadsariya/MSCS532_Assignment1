d8896f35da1b640efad8e072235362720d484a4b
ana-vadsariya <avadsariya32251@ucumberlands.edu>
2025-01-11 12:56:05 -0500

    Fixed the code

--- Insertion Sort.py
+++ Insertion Sort.py
@@ -1,15 +1,13 @@
 def insertion_sort(array):
-    for i in range(1,len(array)):
+    for i in range(0,len(array)):
         temp = array[i]
-        print("temp", temp)
-        for j in range(i-1, 0,-1):
-            print("j", array[j])
+        for j in range(i-1, -1,-1):
             if temp > array[j]:
                 array[j+1] = array[j]
-                array[j + 1] = temp
+                array[j] = temp
     for i in range(len(array)):
         print(array[i], end=" ")
     print()
 
-array = [12, 11, 13, 5, 6]
+array = [1, 12, 3, 44, 5, 6]
 insertion_sort(array)
